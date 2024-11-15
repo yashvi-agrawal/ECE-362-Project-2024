1d4d1fb6e6411698536e729cfd0f7dba7b3e8ac3
Yashvi Agrawal <agraw192@ecelnx79.ecn.purdue.edu>
2024-11-15 02:21:32 -0500

    changed tim7 hz

--- src/main.c
+++ src/main.c
@@ -521,7 +521,7 @@ void setup_tim7()
 {
     RCC->APB1ENR |= RCC_APB1ENR_TIM7EN;
     TIM7 -> PSC = 1000000 - 1;
-    TIM7 -> ARR = 24 - 1;
+    TIM7 -> ARR = 21 - 1;
     TIM7 -> DIER |= TIM_DIER_UIE;
     NVIC -> ISER[0] = 1 << TIM7_IRQn;
     TIM7 -> CR1 |= TIM_CR1_CEN;
