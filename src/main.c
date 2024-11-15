6b369a6da77d10a33b544c13c12b289e79d6ca45
Esharaqaj <143230329+Esharaqaj@users.noreply.github.com>
2024-11-15 06:43:06 -0500

    Update main.c
    two simultaneous arrows falling

--- src/main.c
+++ src/main.c
@@ -1,30 +1,9 @@
-/**
-  ******************************************************************************
-  * @file    main.c
-  * @author  Weili An, Niraj Menon
-  * @date    Feb 7, 2024
-  * @brief   ECE 362 Lab 7 student template
-  ******************************************************************************
-*/
-
-/*******************************************************************************/
-
-// Fill out your username!  Even though we're not using an autotest, 
-// it should be a habit to fill out your username in this field now.
-const char* username = "agraw192";
-
-/*******************************************************************************/ 
 
 #include "stm32f0xx.h"
 #include <stdint.h>
 
 void internal_clock();
 
-// Uncomment only one of the following to test each step
-// #define STEP1
-// #define STEP2
-// #define STEP3
-// #define STEP4
 #define SHELL
 
 void init_usart5() {
@@ -64,193 +43,6 @@ void init_usart5() {
 
 }
 
-#ifdef STEP1
-int main(void){
-    internal_clock();
-    init_usart5();
-    for(;;) {
-        while (!(USART5->ISR & USART_ISR_RXNE)) { }
-        char c = USART5->RDR;
-        while(!(USART5->ISR & USART_ISR_TXE)) { }
-        USART5->TDR = c;
-    }
-}
-#endif
-
-#ifdef STEP2
-#include <stdio.h>
-
-// TODO Resolve the echo and carriage-return problem
-
-int __io_putchar(int c) {
-    // TODO
-    if(c == '\n'){
-        while(!(USART5->ISR & USART_ISR_TXE));
-        USART5->TDR = '\r';
-    }
-    while(!(USART5->ISR & USART_ISR_TXE));
-    USART5->TDR = c;
-    return c;
-}
-
-int __io_getchar(void) {
-    while (!(USART5->ISR & USART_ISR_RXNE));
-    char c = USART5->RDR;
-    // TODO
-    if(c == '\r'){
-        c = '\n';
-    }
-    __io_putchar(c);
-    return c;
-}
-
-int main() {
-    internal_clock();
-    init_usart5();
-    setbuf(stdin,0);
-    setbuf(stdout,0);
-    setbuf(stderr,0);
-    printf("Enter your name: ");
-    char name[80];
-    fgets(name, 80, stdin);
-    printf("Your name is %s", name);
-    printf("Type any characters.\n");
-    for(;;) {
-        char c = getchar();
-        putchar(c);
-    }
-}
-#endif
-
-#ifdef STEP3
-#include <stdio.h>
-#include "fifo.h"
-#include "tty.h"
-int __io_putchar(int c) {
-    // TODO Copy from your STEP2
-    if(c == '\n'){
-        while(!(USART5->ISR & USART_ISR_TXE));
-        USART5->TDR = '\r';
-    }
-    while(!(USART5->ISR & USART_ISR_TXE));
-    USART5->TDR = c;
-    return c;
-}
-
-int __io_getchar(void) {
-    // TODO
-    return line_buffer_getchar();
-}
-
-int main() {
-    internal_clock();
-    init_usart5();
-    setbuf(stdin,0);
-    setbuf(stdout,0);
-    setbuf(stderr,0);
-    printf("Enter your name: ");
-    char name[80];
-    fgets(name, 80, stdin);
-    printf("Your name is %s", name);
-    printf("Type any characters.\n");
-    for(;;) {
-        char c = getchar();
-        putchar(c);
-    }
-}
-#endif
-
-#ifdef STEP4
-
-#include <stdio.h>
-#include "fifo.h"
-#include "tty.h"
-
-// TODO DMA data structures
-#define FIFOSIZE 16
-char serfifo[FIFOSIZE];
-int seroffset = 0;
-
-void enable_tty_interrupt(void) {
-    // TODO
-    USART5->CR1 |= USART_CR1_RXNEIE;
-
-    NVIC_EnableIRQ(USART3_8_IRQn);
-    USART5->CR3 |= USART_CR3_DMAR; 
-
-    RCC->AHBENR |= RCC_AHBENR_DMA2EN;
-    DMA2->CSELR |= DMA2_CSELR_CH2_USART5_RX;
-    DMA2_Channel2->CCR &= ~DMA_CCR_EN;  // First make sure DMA is turned off
-
-    // The DMA channel 2 configuration goes here
-    DMA2_Channel2->CMAR = &serfifo;
-    DMA2_Channel2->CPAR = &(USART5->RDR);
-    DMA2_Channel2->CNDTR |= FIFOSIZE;
-    DMA2_Channel2->CCR &= ~(DMA_CCR_DIR | DMA_CCR_HTIE | DMA_CCR_TCIE);
-    DMA2_Channel2->CCR &= ~(DMA_CCR_MSIZE_1|DMA_CCR_MSIZE_0|DMA_CCR_PSIZE_1|DMA_CCR_PSIZE_0);
-    DMA2_Channel2->CCR |= DMA_CCR_MINC;
-    DMA2_Channel2->CCR &= ~DMA_CCR_PINC;
-    DMA2_Channel2->CCR |= DMA_CCR_CIRC;
-    DMA2_Channel2->CCR &= ~DMA_CCR_MEM2MEM;
-    DMA2_Channel2->CCR |= DMA_CCR_PL_1 | DMA_CCR_PL_0;
-    DMA2_Channel2->CCR |= DMA_CCR_EN;
-}
-
-// Works like line_buffer_getchar(), but does not check or clear ORE nor wait on new characters in USART
-char interrupt_getchar() {
-    // TODO
-    while(fifo_newline(&input_fifo) == 0) {
-        asm volatile ("wfi"); // wait for an interrupt
-    }
-    return fifo_remove(&input_fifo);
-}
-
-int __io_putchar(int c) {
-    // TODO copy from STEP2
-    if(c == '\n'){
-        while(!(USART5->ISR & USART_ISR_TXE));
-        USART5->TDR = '\r';
-    }
-    while(!(USART5->ISR & USART_ISR_TXE));
-    USART5->TDR = c;
-    return c;
-}
-
-int __io_getchar(void) {
-    // TODO Use interrupt_getchar() instead of line_buffer_getchar()
-    return interrupt_getchar();
-}
-
-// TODO Copy the content for the USART5 ISR here
-void USART3_8_IRQHandler(void) {
-    while(DMA2_Channel2->CNDTR != sizeof serfifo - seroffset) {
-        if (!fifo_full(&input_fifo))
-            insert_echo_char(serfifo[seroffset]);
-        seroffset = (seroffset + 1) % sizeof serfifo;
-    }
-}
-
-// TODO Remember to look up for the proper name of the ISR function
-
-int main() {
-    internal_clock();
-    init_usart5();
-    enable_tty_interrupt();
-
-    setbuf(stdin,0); // These turn off buffering; more efficient, but makes it hard to explain why first 1023 characters not dispalyed
-    setbuf(stdout,0);
-    setbuf(stderr,0);
-    printf("Enter your name: "); // Types name but shouldn't echo the characters; USE CTRL-J to finish
-    char name[80];
-    fgets(name, 80, stdin);
-    printf("Your name is %s", name);
-    printf("Type any characters.\n"); // After, will type TWO instead of ONE
-    for(;;) {
-        char c = getchar();
-        putchar(c);
-    }
-}
-#endif
 
 #ifdef SHELL
 #include "commands.h"
@@ -260,9 +52,11 @@ int main() {
 #include "tty.h"
 #include "lcd.h"
 
-int inc = 0;
+int incL = 0;
 int randomIndex = 0; 
-int randomLR;
+int randomIndexR = 0;
+int incR = -1;
+int initial = -1;
 
 // TODO DMA data structures
 #define FIFOSIZE 16
@@ -440,7 +234,7 @@ void initialLCD()
 
 // screen is 240 x 320
 
-void drawUP(int lr, int bw) // lr = left (0) or right (1) side arrow, inc = increment down by inc, bw = black or white
+void drawUP(int inc, int lr, int bw) // lr = left (0) or right (1) side arrow, inc = increment down by inc, bw = black or white
 {
     if (lr == 0){ // left
         LCD_DrawLine(180, 320 - inc, 180, 270 - inc, (bw == 0) ? BLACK : WHITE);
@@ -453,7 +247,7 @@ void drawUP(int lr, int bw) // lr = left (0) or right (1) side arrow, inc = incr
     }
 }
 
-void drawDOWN(int lr, int bw)
+void drawDOWN(int inc, int lr, int bw)
 {
     if (lr == 0) {
         LCD_DrawLine(180, 320 - inc, 180, 270 - inc, (bw == 0) ? BLACK : WHITE); // line
@@ -466,7 +260,7 @@ void drawDOWN(int lr, int bw)
     }
 }
 
-void drawLEFT(int lr, int bw)
+void drawLEFT(int inc, int lr, int bw)
 {
     if (lr == 0) {
         LCD_DrawLine(155, 295 - inc, 205, 295 - inc, (bw == 0) ? BLACK : WHITE);
@@ -479,7 +273,7 @@ void drawLEFT(int lr, int bw)
     }
 }
 
-void drawRIGHT(int lr, int bw)
+void drawRIGHT(int inc, int lr, int bw)
 {
     if (lr == 0) {
         LCD_DrawLine(155, 295 - inc, 205, 295 - inc, (bw == 0) ? BLACK : WHITE);
@@ -495,33 +289,92 @@ void drawRIGHT(int lr, int bw)
 void TIM7_IRQHandler()
 {
     TIM7 -> SR &= ~TIM_SR_UIF;
-    void (*arrowFunctions[])(int, int) = {drawUP, drawDOWN, drawLEFT, drawRIGHT};
+    void (*arrowFunctions[])(int, int, int) = {drawUP, drawDOWN, drawLEFT, drawRIGHT};
 
-    (*arrowFunctions[randomIndex])(randomLR, 1);
+    (*arrowFunctions[randomIndex])(incL,0, 1);
+    (*arrowFunctions[randomIndexR])(incR,1, 1);
 
-    inc++;
+    incL++;
+    incR++;
 
-    if (inc == 270)
+    // if (inc == 270)
+    // {
+    //     inc = 0;
+
+    // }
+    // else if ((inc-1) == 0) {
+    //     randomIndex = rand() % 4;
+    //     (*arrowFunctions[randomIndex])(0, 0);
+    // }
+    // else
+    // {
+    //     (*arrowFunctions[randomIndex])(0, 0);
+    // }
+
+    // if(inc == 100 && initial == 0)
+    // {
+    //     initial = 1;
+    //     incR = 1;
+    // }
+
+    // if((incR - 1)== 0)
+    // {
+    //     randomIndexR = rand() % 4;
+    //     (*arrowFunctions[randomIndexR])(1, 0);
+    // }
+    // else if(incR == 270)
+    // {
+    //     incR = 0;
+    // }
+    // else{
+    //     (*arrowFunctions[randomIndexR])(1, 0);
+    // }
+
+
+    if((incL - 1) == 0)
     {
-        inc = 0;
-        return;
-    }
-    if ((inc-1) == 0) {
         randomIndex = rand() % 4;
-        randomLR = rand() % 2;
-        (*arrowFunctions[randomIndex])(randomLR, 0);
+        (*arrowFunctions[randomIndex])(incL,0, 0);
+    }
+    else if(incL == 270)
+    {
+        incL = 0;
     }
     else
     {
-        (*arrowFunctions[randomIndex])(randomLR, 0);
+        (*arrowFunctions[randomIndex])(incL, 0, 0);
     }
+
+    if(incL == 150 && initial == -1)
+    {
+        initial = 0;
+        incR = 1;
+    }
+
+    if(initial == 0)
+    {
+    if((incR - 1) == 0)
+        {
+            randomIndexR = rand() % 4;
+            (*arrowFunctions[randomIndexR])(incR, 1, 0);
+        }
+    else if(incR == 270)
+        {
+            incR = 0;
+        }
+    else
+        {
+            (*arrowFunctions[randomIndexR])(incR, 1, 0);
+        }
+    }
+    // hi
 }
 
 void setup_tim7()
 {
     RCC->APB1ENR |= RCC_APB1ENR_TIM7EN;
     TIM7 -> PSC = 1000000 - 1;
-    TIM7 -> ARR = 21 - 1;
+    TIM7 -> ARR = 55 - 1;
     TIM7 -> DIER |= TIM_DIER_UIE;
     NVIC -> ISER[0] = 1 << TIM7_IRQn;
     TIM7 -> CR1 |= TIM_CR1_CEN;
@@ -549,3 +402,4 @@ int main() {
     // workerLCD();
 
 }
+
